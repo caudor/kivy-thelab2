39232af627027a5ac8ebf4af76ee31ffe621dbd4
chris <chris4sawit@gmail.com>
2021-07-02 05:45:03 -0500

    All working except widgets showing blank

--- widget_examples.py
+++ widget_examples.py
@@ -4,13 +4,12 @@ from kivy.uix.gridlayout import GridLayout
 
 Builder.load_file("widget_examples.kv")
 
-class WidgetsExample(GridLayout):
+
+class WidgetExamples(GridLayout):
     count = 1
     count_enabled = BooleanProperty(False)
-    #slider_enabled = BooleanProperty(False)
     my_text = StringProperty("1")
     txt_input_str = StringProperty("foo")
-    #slider_value_txt = StringProperty("50")
 
     def on_button_click(self):
         print('button clicked')
@@ -29,11 +28,5 @@ class WidgetsExample(GridLayout):
             widget.text = "ON"
             self.count_enabled = True
 
-    #def on_switch_active(self, widget):
-    #    print("switch: " + str(widget.active))
-
-    #def on_slider_value(self, widget):
-    #   print("slider value: " + str(int(widget.value)))
-    #   self.slider_value_txt =  str(int(widget.value))
     def on_text_validate(self, widget):
         self.txt_input_str = widget.text
